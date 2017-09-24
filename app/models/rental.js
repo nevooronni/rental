6befff20e8c4d969fdb263706bb315e520ebc3f4
Neville Oronni <nevooronni@gmail.com>
2017-09-24 17:01:12 +0300

    added details functionality and relationships between the rental and review models

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	owner: DS.attr(),
-	city: DS.attr(),
-	tfype: DS.attr(),
-	image: DS.attr(),
-	bedrooms: DS.attr()
+  owner: DS.attr(),
+  city: DS.attr(),
+  type: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr(),
+  reviews: DS.hasMany('review', { async: true })
 });
