b1f437cfe1e92dbd5588ac4eb85c6b5a4eaf8260
Neville Oronni <nevooronni@gmail.com>
2017-09-24 14:10:39 +0300

    added review models

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,15 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');//reference the database and find all records of type rental using the findAll method and return them.
-  },
+  return Ember.RSVP.hash({
+    rentals: this.store.findAll('rental'),
+    reviews: this.store.findAll('review')
+  });
+},
   actions: {
   	saveRental3(params) {
   		var newRental = this.store.createRecord('rental', params);
   		newRental.save();
-  		this.transitionTo('index');
-  	},
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          rental.set(key,params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-  	destroyRental(rental) {
-  		rental.destroyRecord();
   		this.transitionTo('index');
   	}
   }
